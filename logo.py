b14584b2a774d955b811c9e07a70a212214e78df
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-06 01:37:16 +0200

    ADD logo information

--- logo.py
+++ logo.py
@@ -3,3 +3,8 @@ from PIL import Image
 ## ADD logo , new folder 
 LOGO_NAME = input('Enter The Logo Nmae With Extension: ')
 NEW_FOLDER_NAME =input('Enter the new folder name: ')
+
+
+## ADD logo information
+logo_image =Image.open( LOGO_NAME)
+logo_width , logo_heigh = logo_image.size
