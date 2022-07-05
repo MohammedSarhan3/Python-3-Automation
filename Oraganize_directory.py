6fa7f076b4f7bff7adb94c898efa6e2822594520
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-05 22:53:00 +0200

    organize App files into Apps folder

--- Oraganize_directory.py
+++ Oraganize_directory.py
@@ -30,3 +30,9 @@ for filename in os.listdir(current_dir):
             os.mkdir("Documents")
         shutil.copy(filename, "Documents")
         os.remove(filename)
+#organize App files into Apps folder
+    if filename.endswith((".exe",".dng")):
+        if not os.path.exists("Apps"):
+            os.mkdir("Apps")
+        shutil.copy(filename, "Apps")
+        os.remove(filename)
