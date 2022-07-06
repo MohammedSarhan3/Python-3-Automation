4cb2a6378e44b114c67505be20a12be2fe01c160
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-06 23:19:31 +0200

    #Extract CSV Files from Websites

---
+++ Table extraction/exract_table.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#Extract Tables from Websites
+simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes')
+print(len(simpsons))
+print(simpsons[1])
+
+
+#Extract CSV Files from Websites
+#reasing 1 csv file from the website
+df_premire21=pd.read_csv("https://www.football-data.co.uk/mmz4281/2122/E0.csv")
