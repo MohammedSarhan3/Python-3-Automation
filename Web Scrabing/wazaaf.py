2faee564810dcfe4f78ab2773bd2473533b5a6a2
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-17 23:26:38 +0200

    1st step install and import modules

---
+++ Web Scrabing/wazaaf.py
@@ -0,0 +1,10 @@
+# 1st step install and import modules
+import requests
+from bs4 import BeautifulSoup
+import csv
+from itertools import zip_longest
+
+job_title =[]
+company_name = []
+locations_name = []
+skills = []
