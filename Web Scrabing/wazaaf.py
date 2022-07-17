d50db2bc7b75d903a1f0129324d5a0353073e108
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-17 23:29:48 +0200

    5th step find the elements containing info we need

--- Web Scrabing/wazaaf.py
+++ Web Scrabing/wazaaf.py
@@ -20,3 +20,10 @@ src = result.content
 
 #4th step creat soup object to parse content
 soup = BeautifulSoup(src, "lxml")
+
+
+# 5th step find the elements containing info we need
+job_titles = soup.find_all("h2", {"class":"css-m604qf"} )
+company_names = soup.find_all("a",{"class":"css-17s97q8"})
+locations_names = soup.find_all("span",{"class":"css-5wys0k"})
+jop_skills = soup.find_all("div",{"class":"css-y4udm8"})
