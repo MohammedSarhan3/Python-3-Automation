a51d23af0269d8c2d635e1ad7077166338ba54a5
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-17 23:30:25 +0200

    6th step loop over returned lists to extrect needed info into other lists

--- Web Scrabing/wazaaf.py
+++ Web Scrabing/wazaaf.py
@@ -27,3 +27,12 @@ job_titles = soup.find_all("h2", {"class":"css-m604qf"} )
 company_names = soup.find_all("a",{"class":"css-17s97q8"})
 locations_names = soup.find_all("span",{"class":"css-5wys0k"})
 jop_skills = soup.find_all("div",{"class":"css-y4udm8"})
+
+
+# 6th step loop over returned lists to extrect needed info into other lists
+for i in range(len(job_titles)):
+    job_title.append(job_titles[i].text)
+    company_name.append(company_names[i].text)
+    locations_name.append(locations_names[i].text)
+    skills.append(jop_skills[i].text)
+#print(job_title,company_name,locations_name, skills)
